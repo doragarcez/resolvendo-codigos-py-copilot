5e5bf744fc6497eafa405ca2778a953fa58cf2db
doragarcez <garcezalves.isadora@gmail.com>
2025-06-25 15:52:45 -0300

    Resposta?

--- resolucoes_code/repet_txt.py
+++ resolucoes_code/repet_txt.py
@@ -1,1 +1,8 @@
 # Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
+
+primeiro_numero = int(input("Digite um número: "))
+segundo_numero = int(input("Digite outro número para realizar a soma: "))
+
+resultado = primeiro_numero + segundo_numero
+
+print("O resultado da soma é igual a", resultado)
