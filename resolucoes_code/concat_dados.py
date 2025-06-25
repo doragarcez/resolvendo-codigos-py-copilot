29f3bac5185d3f0c06b85c7a050f792424b257ed
doragarcez <garcezalves.isadora@gmail.com>
2025-06-25 15:45:36 -0300

    Resolução do Desafio de Concatenação

--- resolucoes_code/concat_dados.py
+++ resolucoes_code/concat_dados.py
@@ -1,1 +1,8 @@
 # Vamos receber dois dados diferentes do usuário e concatena-los em uma única string?!
+
+nome = input("Digite apenas seu nome: ")
+sobrenome = input("Digite seu sobrenome: ")
+
+nome_completo = nome + " " + sobrenome
+
+print ("Seu nome completo é:", nome_completo)
